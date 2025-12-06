b6f9f827eb55c5525391f70a32440d9dd537a793
MikelMayer <myhahail@gmail.com>
2025-12-06 16:45:31 +0200

    Add title

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -12,6 +12,7 @@ function MainPage() {
   return (
     <div>
       <Title />
+      <h1>Music Player</h1>
       <div style={{ display: "flex", gap: "20px" }}>
         <TracksList onTrackSelect={handleClick} selectedTrackId={trackId} />
         <TrackDetails selectedTrackId={trackId} />
